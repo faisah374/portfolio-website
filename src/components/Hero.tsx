668d0ec71979b073a2e861774255a73c734e7533
Faisal Hameed <faisalh374@gmail.com>
2024-11-08 14:58:47 +0500

    final

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,7 @@ const Hero = () => {
    <div className='hidden lg:block'></div>
    <div className='text-[80px] sm:text-[100px] font-bold leading-tight flex justify-center items-center'>
     <div>
-      <p data-aos="zoom-in-up">I'm</p>
+      <p data-aos="zoom-in-up">I am</p>
     <p data-aos ="zoom-in-up">Faisal</p>
     <p  data-aos="zoom-in-left">Hameed</p>
     </div>
